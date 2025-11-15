4bb6141950abfb20c8ebdda0855bd3e8962ef7e4
om-gupta-30 <guptaom31619@gmail.com>
2025-11-15 12:08:05 +0530

    Remove app.run for Render deployment

--- python_service/app.py
+++ python_service/app.py
@@ -332,5 +332,5 @@ def health():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run( host='0.0.0.0', port=5001)
 
